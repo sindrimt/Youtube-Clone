e6cb35ee8c5b8115809f9d7b58d91ec9004e020a
sindrimt <sindrimt@stud.ntnu.no>
2022-03-21 08:16:48 +0100

    Comments and stuff

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
+
 import Card from "../../components/Card/Card";
+import { GridContainer, Outer } from "./ContentStyles";
 
 import axios from "axios";
-import styled from "styled-components";
-
-import { GridContainer, ContentGap, Outer } from "./ContentStyles";
 
 import "./content.css";
-import { ProfilePicture } from "../Card/CardStyles";
 
 const Content: React.FC = () => {
   const [isLoading, setIsLoading] = useState<Boolean>(true);
@@ -36,22 +34,29 @@ const Content: React.FC = () => {
         // Gives information about the channel
         const profileUrl = `https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${video.snippet.channelId}&key=${API_KEY}`;
 
-        // Initiates videoTuple with videoID and an empty array
+        // Sets the key in the Map to the videoId
         videoTuple.set(video.id.videoId, []);
 
         // Two return statements separated by comma
+        // The first return statement fills up the first position in the array in the Map with res.data
+        // The second return statement fills up the second position in the array in the Map with the profile picture
+
+        /**  The return statements does not run synchronously, but when the loop is done, the first and second
+           positions in the array will be filled up in the correct order since the return statements are independent **/
+
         return (
           axios.get(url).then((res) => {
             videoTuple.get(video.id.videoId)[0] = res.data;
+            console.log(video.id.videoId);
           }),
           axios.get(profileUrl).then((res) => {
-            // Pusher profile picture url til arrayen i Mapet
             videoTuple.get(video.id.videoId)[1] = res.data.items[0].snippet.thumbnails.default.url;
+            console.log(video.id.videoId);
           })
         );
       });
 
-      // When all request are finished
+      // When all request are finished inside requests
       return Promise.all(requests).then(() => {
         const posts: any = [];
         const profilePictures: any = [];
@@ -67,7 +72,7 @@ const Content: React.FC = () => {
       });
     });
   };
-
+  // Displaying loading while waiting for fetching data
   if (isLoading) {
     return (
       <section className="outer">
