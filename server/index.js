50c96c41367a3530a2b6dfa94ed4d1f3b7583aa7
sindrimt <sindrimt@stud.ntnu.no>
2022-03-31 20:07:47 +0200

    mongodb connection

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// prefixes api with /api
+
+app.use("/api", postRoutes);
+
+const CONNECTION_URL =
+  "mongodb+srv://sindrimt:SvampeBob7@cluster0.9theg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
+  .catch((error) => console.log(error.message));
