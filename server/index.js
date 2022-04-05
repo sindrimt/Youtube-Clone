53772adfc3de28b6c5c5eae16d5f66662e47ded4
sindrimt <sindrimt@gmail.com>
2022-04-05 13:18:52 +0200

    kk

--- server/index.js
+++ server/index.js
@@ -37,9 +37,11 @@ app.use("/api", userRoutes);
   res.send("Api home page");
 }); */
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../build"));
-}
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 mongoose
   .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
