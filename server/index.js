854e29f84c0b05f78a42ebdd623fcf6167f49771
sindrimt <sindrimt@gmail.com>
2022-04-05 10:48:14 +0200

    environment vars

--- server/index.js
+++ server/index.js
@@ -3,9 +3,14 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 import userRoutes from "./routes/users.js";
 
 const app = express();
+
+const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -19,11 +24,7 @@ app.get("/", (req, res) => {
   res.send("Home page");
 });
 
-const CONNECTION_URL =
-  "mongodb+srv://sindrimt:SvampeBob7@cluster0.9theg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 8000;
-
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
   .catch((error) => console.log(error.message));
