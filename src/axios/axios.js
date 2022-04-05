c0263dd2575ee60d492c5f20ccc36f12d5b51b25
sindrimt <sindrimt@gmail.com>
2022-04-05 19:07:55 +0200

    asd

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = "/api/users";
+const apiUrl = "http://localhost:8000/api/users";
 
 export const getUser = () => axios.get(apiUrl).then((res) => console.log(res));
 
