74debdbc310fecb3bab36cee6d413c1eb1615453
sindrimt <sindrimt@gmail.com>
2022-04-09 21:16:06 +0200

    Profile pic when logging in

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import Search from "../Search/Search";
 
-import { Div2, Div3, NavContainer } from "./NavbarStyles";
+import { Div2, Div3, NavContainer, LoginButton, ProfilePic } from "./NavbarStyles";
+import { CgProfile } from "react-icons/cg";
+import { IoPersonCircleOutline } from "react-icons/io5";
+
 import {
   signInWithGoogle,
   signOutWithGoogle,
@@ -16,10 +19,16 @@ import axios from "axios";
 
 import "./navbar.css";
 import { State } from "../../state/reducers";
+import { changeUrl, setUser } from "../../state/actions/users";
 
 const Navbar = () => {
   const [done, setDone] = useState<boolean>(false);
+  const [loadingUser, setLoadingUser] = useState<boolean>(false);
   const users = useSelector((state: State) => state.users);
+
+  const dispatch = useDispatch();
+
+  const user = useSelector((state: State) => state.user);
 
   console.log(users);
   //todo DET KAN HENDE USER ER KNYTTET TIL URL OG DERFOR FORURSAKER REFRESH?
@@ -27,8 +36,11 @@ const Navbar = () => {
   // Gets results (user data) from sign in with redirect when finished
   useEffect(() => {
     getGoogleRedirectResults()
-      .then((res) => {
-        console.log(res);
+      .then(({ user }: any) => {
+        //todo
+        dispatch(setUser(user));
+        setLoadingUser(false);
+        //console.log(res);
         console.log("Loaded sign in data");
       })
       .catch((error) => console.log(error));
@@ -36,9 +48,15 @@ const Navbar = () => {
 
   // Sign in with google redirect
   const handleLogin = () => {
+    // Sets state to empty, for rendering purposes
+    //dispatch(setUser({ "": "" }));
+
     signInWithGoogleRedirect()
       .then(() => console.log("Redirecting"))
-      .finally(() => setDone(true));
+      .finally(() => {
+        setLoadingUser(false);
+        setDone(true);
+      });
   };
 
   // Sign out with google, sets user state to null
@@ -60,17 +78,32 @@ const Navbar = () => {
     postUser({ username: "gamer", email: "yoyoyo", profilePic: "dette er url.com" });
   };
 
+  const handleState = () => {
+    console.log(user);
+  };
+
   return (
     <NavContainer>
       <Div2>
         <Search />
       </Div2>
       <Div3>
-        <button onClick={handleLogin}>Sign In</button>
+        <button onClick={handleState}>Check state</button>
+        {/* <button onClick={handleLogin}>Sign In</button>
         <button onClick={handleLogout}>Sign Out</button>
-        {/* <button onClick={() => console.log(user)}>Check state</button> */}
         <button onClick={handleGetUser}>Get User</button>
-        <button onClick={handlePostUser}>Post User</button>
+        <button onClick={handlePostUser}>Post User</button> */}
+
+        {Object.keys(user).length === 0 && user.constructor === Object ? (
+          <LoginButton onClick={handleLogin}>
+            <span style={{ padding: "7px" }}>
+              <IoPersonCircleOutline color="#3EA6FF" size={25} />
+            </span>
+            LOGG PÅ
+          </LoginButton>
+        ) : (
+          <ProfilePic src={user.photoURL} />
+        )}
       </Div3>
     </NavContainer>
   );
--- frontend/src/components/Navbar/NavbarStyles.js
+++ frontend/src/components/Navbar/NavbarStyles.js
@@ -32,3 +32,27 @@ export const Div3 = styled.div`
   justify-content: space-around;
   align-items: center;
 `;
+
+export const ProfilePic = styled.img`
+  width: 33px;
+  height: 33px;
+  border-radius: 50%;
+  object-fit: contain;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const LoginButton = styled.button`
+  display: flex;
+  align-items: center;
+  color: #3ea6ff;
+  width: 110px;
+  height: 40px;
+  background-color: transparent;
+  border-radius: 1px;
+  border: 1px solid #3ea6ff;
+  &:hover {
+    cursor: pointer;
+  }
+`;
