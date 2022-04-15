87781c7b413956aba0590dadc71fe8f8af48367f
sindrimt <sindrimt@gmail.com>
2022-04-15 23:56:06 +0200

    Width change

--- frontend/src/components/SideContainer/SideContentStyles.js
+++ frontend/src/components/SideContainer/SideContentStyles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Sidebar = styled.div`
   height: 100vh;
-  width: 230px;
+  width: 240px;
   position: fixed;
   display: inline-block;
   top: 0;
