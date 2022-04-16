bdc967f94e54ce423ded2ad607673cc23bfd3bf7
sindrimt <sindrimt@gmail.com>
2022-04-16 23:24:35 +0200

    More content and Scrollbar

--- frontend/src/components/SideContainer/SideContainer.tsx
+++ frontend/src/components/SideContainer/SideContainer.tsx
@@ -2,11 +2,24 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
 
-import { Sidebar, SidebarSmall, Logo, SubscribedOuter, SubscribedOuter2, Subscribed, Line } from "./SideContentStyles";
+import {
+  Sidebar,
+  SidebarSmall,
+  Logo,
+  SubscribedOuter,
+  SubscribedOuter2,
+  Subscribed,
+  Line,
+  SubscribedOuter3,
+  SubProfilePicture,
+} from "./SideContentStyles";
 
 import { AiOutlineMenu } from "react-icons/ai";
 import { BsFillHouseDoorFill } from "react-icons/bs";
-import { RiCompass3Line, RiYoutubeFill, RiYoutubeLine } from "react-icons/ri";
+import { RiCompass3Line, RiYoutubeFill, RiYoutubeLine, RiVideoLine } from "react-icons/ri";
+import { MdOutlineVideoLibrary } from "react-icons/md";
+import { BsClockHistory, BsClock, BsChevronDown } from "react-icons/bs";
+import { BiLike } from "react-icons/bi";
 
 import logo from "../../assets/ytlogotr.png";
 import "./sidecontainer.css";
@@ -17,7 +30,7 @@ const SideContainer = () => {
 
   //todo SUBSCRIPTION API URL
   //todo ==============================
-  //https://www.googleapis.com/youtube/v3/subscriptions?key=AIzaSyB59He1O3kiRo6FXq0XZ9klPPl300Wy_yw&part=snippet&channelId=UC0sEIyXXalzD1lbwm3D2xpA
+  //https://www.googleapis.com/youtube/v3/subscriptions?key=AIzaSyB59He1O3kiRo6FXq0XZ9klPPl300Wy_yw&part=snippet&channelId=UC0sEIyXXalzD1lbwm3D2xpA&maxResults=8
   //todo ==============================
   //todo SUBSCRIPTION API URL
 
@@ -35,10 +48,10 @@ const SideContainer = () => {
   return (
     <>
       <LoadingBar color="#f11946" progress={progress} onLoaderFinished={() => setProgress(0)} />
+      <Logo src={logo} onClick={logoClick} />
+      <AiOutlineMenu color="white" size={20} className="hamburger_menu" onClick={handleClick} />
       {expand ? (
         <Sidebar>
-          <AiOutlineMenu color="white" size={20} className="hamburger_menu" onClick={handleClick} />
-          <Logo src={logo} onClick={logoClick} />
           <SubscribedOuter>
             <Subscribed>
               <BsFillHouseDoorFill color="white" size={21} /> <span className="textSpan">Startside</span>
@@ -53,21 +66,73 @@ const SideContainer = () => {
               <RiYoutubeLine color="white" size={22} /> <span className="textSpan">Abbonomenter</span>
             </Subscribed>
           </SubscribedOuter>
+
           <Line />
+
           <SubscribedOuter2>
             <Subscribed>
-              <BsFillHouseDoorFill color="white" size={21} /> <span className="textSpan">Startside</span>
+              <MdOutlineVideoLibrary color="white" size={21} /> <span className="textSpan">Bibliotek</span>
             </Subscribed>
             <Subscribed>
-              <RiCompass3Line color="white" size={24} /> <span className="textSpan">Utforsk</span>
+              <BsClockHistory color="white" size={22} /> <span className="textSpan">Logg</span>
             </Subscribed>
             <Subscribed>
-              <RiYoutubeFill color="white" size={22} /> <span className="textSpan">Shorts</span>
+              <RiVideoLine color="white" size={22} /> <span className="textSpan">Videoene dine</span>
             </Subscribed>
             <Subscribed>
-              <RiYoutubeLine color="white" size={22} /> <span className="textSpan">Abbonomenter</span>
+              <BsClock color="white" size={21} /> <span className="textSpan">Se senere</span>
+            </Subscribed>
+            <Subscribed>
+              <BiLike color="white" size={21} /> <span className="textSpan">Likte videoer</span>
+            </Subscribed>
+            <Subscribed>
+              <BsChevronDown color="white" size={15} /> <span className="textSpanArrow">Vis mer</span>
             </Subscribed>
           </SubscribedOuter2>
+
+          <Line />
+
+          <SubscribedOuter3>
+            <Subscribed>
+              <span className="abbonomenter">ABBONOMENTER</span>
+            </Subscribed>
+            <Subscribed>
+              <SubProfilePicture src="https://semprefuigeek.com.br/wp-content/uploads/2022/04/one-piece-luffy-gear-5-fanart-autor-desconhecido-postcover-1.jpg" />
+              <span className="textSpanSub">Channel Name</span>
+            </Subscribed>
+            <Subscribed>
+              <SubProfilePicture src="https://semprefuigeek.com.br/wp-content/uploads/2022/04/one-piece-luffy-gear-5-fanart-autor-desconhecido-postcover-1.jpg" />
+              <span className="textSpanSub">Channel</span>
+            </Subscribed>
+            <Subscribed>
+              <SubProfilePicture src="https://semprefuigeek.com.br/wp-content/uploads/2022/04/one-piece-luffy-gear-5-fanart-autor-desconhecido-postcover-1.jpg" />
+              <span className="textSpanSub">Channel23</span>
+            </Subscribed>
+            <Subscribed>
+              <SubProfilePicture src="https://semprefuigeek.com.br/wp-content/uploads/2022/04/one-piece-luffy-gear-5-fanart-autor-desconhecido-postcover-1.jpg" />
+              <span className="textSpanSub">Channel Name3</span>
+            </Subscribed>
+            <Subscribed>
+              <SubProfilePicture src="https://semprefuigeek.com.br/wp-content/uploads/2022/04/one-piece-luffy-gear-5-fanart-autor-desconhecido-postcover-1.jpg" />
+              <span className="textSpanSub">Name Name</span>
+            </Subscribed>
+            <Subscribed>
+              <SubProfilePicture src="https://semprefuigeek.com.br/wp-content/uploads/2022/04/one-piece-luffy-gear-5-fanart-autor-desconhecido-postcover-1.jpg" />
+              <span className="textSpanSub">Name</span>
+            </Subscribed>
+            <Subscribed>
+              <SubProfilePicture src="https://semprefuigeek.com.br/wp-content/uploads/2022/04/one-piece-luffy-gear-5-fanart-autor-desconhecido-postcover-1.jpg" />
+              <span className="textSpanSub">Channel Name</span>
+            </Subscribed>
+            <Subscribed>
+              <SubProfilePicture src="https://semprefuigeek.com.br/wp-content/uploads/2022/04/one-piece-luffy-gear-5-fanart-autor-desconhecido-postcover-1.jpg" />
+              <span className="textSpanSub">Channel Name</span>
+            </Subscribed>
+            <Subscribed>
+              <SubProfilePicture src="https://semprefuigeek.com.br/wp-content/uploads/2022/04/one-piece-luffy-gear-5-fanart-autor-desconhecido-postcover-1.jpg" />
+              <span className="textSpanSub">Channel Name</span>
+            </Subscribed>
+          </SubscribedOuter3>
         </Sidebar>
       ) : (
         <SidebarSmall>
--- frontend/src/components/SideContainer/SideContentStyles.js
+++ frontend/src/components/SideContainer/SideContentStyles.js
@@ -1,13 +1,29 @@
 import styled from "styled-components";
 
 export const Sidebar = styled.div`
-  height: 100vh;
+  margin-top: 55px;
+  height: 92.7vh;
   width: 240px;
   position: fixed;
+  overflow: hidden;
   display: inline-block;
   top: 0;
   left: 0;
-  background-color: rgba(33, 33, 33, 0.98);
+  background-color: #212121;
+  &:hover {
+    overflow-y: scroll;
+
+    ::-webkit-scrollbar {
+      width: 16px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      border: 4px solid rgba(0, 0, 0, 0);
+      background-clip: padding-box;
+      border-radius: 9999px;
+      background-color: #717171;
+    }
+  }
 
   @media (max-width: 1330px) {
     height: 100vh;
@@ -34,7 +50,7 @@ export const SidebarSmall = styled.div`
 
 export const Logo = styled.img`
   width: 90px;
-  position: absolute;
+  position: fixed;
   top: 16px;
   left: 72px;
   &:hover {
@@ -44,19 +60,15 @@ export const Logo = styled.img`
 
 export const SubscribedOuter = styled.div`
   display: flex;
-  padding: 20px 0px 15px 25px;
-  margin-top: 52px;
+  padding: 20px 0px 20px 25px;
   flex-direction: column;
   align-items: inherit;
   width: 100%;
   gap: 18px;
-  /* @media (max-width: 1330px) {
-    display: none;
-  } */
 `;
 export const SubscribedOuter2 = styled.div`
   display: flex;
-  padding: 20px 0px 0px 25px;
+  padding: 20px 0px 25px 25px;
   flex-direction: column;
   align-items: inherit;
   width: 100%;
@@ -64,6 +76,25 @@ export const SubscribedOuter2 = styled.div`
   @media (max-width: 1330px) {
     display: none;
   }
+`;
+
+export const SubscribedOuter3 = styled.div`
+  display: flex;
+  padding: 25px 0px 15px 25px;
+  flex-direction: column;
+  align-items: inherit;
+  width: 100%;
+  gap: 18px;
+  @media (max-width: 1330px) {
+    display: none;
+  }
+`;
+
+export const SubProfilePicture = styled.img`
+  border-radius: 50%;
+  width: 25px;
+  height: 25px;
+  object-fit: cover;
 `;
 
 export const Subscribed = styled.div`
