b26ee58c8c9fb9fc9b644de0f07c33736ba0b7e5
sindrimt <sindrimt@gmail.com>
2022-04-22 22:16:01 +0200

    new filter comp and styles

---
+++ frontend/src/components/Filters/SearchFilter.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Line, Text } from "./SearchFilterStyles";
+
+const SearchFilter = () => {
+  return (
+    <>
+      <Text>Filtre</Text>
+      <Line />
+    </>
+  );
+};
+
+export default SearchFilter;
---
+++ frontend/src/components/Filters/SearchFilterStyles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Line = styled.div`
+  height: 1px;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0.15);
+  margin-bottom: 20px;
+`;
+export const Text = styled.span`
+  color: white;
+  font-size: 14px;
+`;
