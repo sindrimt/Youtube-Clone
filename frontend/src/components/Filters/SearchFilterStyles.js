0ffec5efa3de6ea0a3fed1ca9d17d74c6a0f13b4
sindrimt <sindrimt@gmail.com>
2022-04-29 17:37:35 +0200

    Outer and inner container

--- frontend/src/components/Filters/SearchFilterStyles.js
+++ frontend/src/components/Filters/SearchFilterStyles.js
@@ -6,7 +6,14 @@ export const Line = styled.div`
   background-color: rgba(255, 255, 255, 0.15);
   margin-bottom: 20px;
 `;
-export const Text = styled.span`
-  color: white;
+export const Text = styled.div`
+  color: gray;
   font-size: 14px;
+  margin-left: 8px;
+`;
+
+export const FilterDiv = styled.div`
+  display: flex;
+  align-items: center;
+  padding-bottom: 10px;
 `;
